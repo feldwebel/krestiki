fdf936c3280e0df4ba6a666e4eb9583faa24cf40
Dan Tereschenko <daniel@tereschenko.info>
2016-05-29 09:47:02 +0300

    fix

--- htdocs/engine/TurnAction.php
+++ htdocs/engine/TurnAction.php
@@ -65,14 +65,14 @@ class TurnAction implements IAction {
      */
     private function isWin(array $position, $p, $row, $col)
     {
-        $streak1 = $streak2 = 0;
+        $streak1 = $streak2 = 0; // + check
         for ($i = 0; $i <= self::SIZE; $i++) {
             $streak1 = ($position[$row][$i] === $p) ? $streak1 + 1 : 0;
             $streak2 = ($position[$i][$col] === $p) ? $streak2 + 1 : 0;
             if ($streak1 == self::STREAK || $streak2 == self::STREAK) return true;
         }
 
-        for ($n = 0; $n <= self::SIZE - self::STREAK; $n++) {
+        for ($n = 0; $n <= self::SIZE - self::STREAK; $n++) { // \\\\ check
             $streak1 = $streak2 = 0;
             for ($m = 0; $m <= self::SIZE - $n; $m++) {
                 $streak1 = ($position[$m][$m + $n] === $p) ? $streak1 + 1 : 0;
@@ -81,7 +81,7 @@ class TurnAction implements IAction {
             }
         }
 
-        for ($n = self::STREAK-1; $n <= self::SIZE; $n++) {
+        for ($n = self::STREAK-1; $n <= self::SIZE; $n++) { // //// check
             $streak1 = $streak2 = 0;
             for ($m = 0; $m <= $n; $m++) {
                 $streak1 = ($position[$m][$n - $m] === $p) ? $streak1 + 1 : 0;
